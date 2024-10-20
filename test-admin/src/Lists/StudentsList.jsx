1a3248e04c325d013c9b5543d409ada42931d664
Lwazi Cekiso <132575366+Lwazicekiso@users.noreply.github.com>
2024-10-20 23:22:30 +0000

    Added Module fields in students and added fields to list to view them

--- test-admin/src/Lists/StudentsList.jsx
+++ test-admin/src/Lists/StudentsList.jsx
@@ -1,4 +1,4 @@
-import { Datagrid, Edit, Create, SimpleForm,DateField, List, Show,Toolbar, SimpleShowLayout ,TextField,  TextInput  } from 'react-admin';
+import { Datagrid, Edit, Create, SimpleForm,DateField, List, Show,Toolbar, SimpleShowLayout ,TextField,  TextInput,ReferenceField  } from 'react-admin';
 import {Filter } from 'react-admin';
 import {dataGridStyle,commonStyles} from './ListStyle'
 
@@ -10,6 +10,16 @@ export const StudentList = () => (
             <TextField source="StudentNumber" />
             <TextField source="name" />
             <TextField source="surname" />
+            {/* Fields for modules*/}
+            <ReferenceField source="Module_1" reference="Module" label="Module 1">
+                <TextField source="name" />
+            </ReferenceField>
+            <ReferenceField source="Module_2" reference="Module" label="Module 2">
+                <TextField source="name" />
+            </ReferenceField>
+            <ReferenceField source="Module_3" reference="Module" label="Module 3">
+                <TextField source="name" />
+            </ReferenceField>
         </Datagrid>
     </List>
 );
@@ -55,10 +65,18 @@ export const StudentShow = () => (
         marginTop: '150px', // Adjust to prevent overlap with the AppBar (200px height + marginBottom)
     }}>
         <SimpleShowLayout>
-            <TextField source="id" label="ID" />
-            <TextField source="StudentID" label="Student ID" />
+            <TextField source="StudentNumber" label="Student Number" />
             <TextField source="name" label="Name" />
             <TextField source="surname" label="Surname" />
+            <ReferenceField source="Module_1" reference="Module" label="Module 1">
+                <TextField source="name" />
+            </ReferenceField>
+            <ReferenceField source="Module_2" reference="Module" label="Module 2">
+                <TextField source="name" />
+            </ReferenceField>
+            <ReferenceField source="Module_3" reference="Module" label="Module 3">
+                <TextField source="name" />
+            </ReferenceField>
             <DateField source="created" label="Created Date" />
             <DateField source="updated" label="Last Updated" />
         </SimpleShowLayout>
